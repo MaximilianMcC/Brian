8910ca970d43f84793f8a1565bd265f4b81f340b
Max <max@maximilian.co.nz>
2022-12-17 22:12:00 +1300

    Added info and github commands

--- main.py
+++ main.py
@@ -23,6 +23,42 @@ async def test(ctx):
 
 
 
+# Info command
+@brian.slash_command(description="Get information about Brian")
+async def info(ctx):
+
+    # Information embed
+    description = """
+    Brian is a multipurpose Discord bot. There isn't really a set theme or idea for the bot. Whenever I think of a command I just add it to Brian.
+
+    You can use the `/github` command to view Brian's Github repository
+    """
+    embed = discord.Embed(title="Brian Information", description=description)
+
+    # Send the embed
+    await ctx.respond(embed=embed)
+
+
+# Github command
+@brian.slash_command(description="View Brain's github repo")
+async def github(ctx):
+
+    # Make the embed
+    description = """
+    Brian is an open source Discord bot. This means people can read all of Brian's code.
+
+    Click the button below to visit the repo.
+    """
+    embed = discord.Embed(title="GitHib link", description=description)
+    embed.set_thumbnail(url="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png")
+
+    # Make a button with the github link
+    view = discord.ui.View()
+    view.add_item(discord.ui.Button(label="GitHub Repo Link", url="https://github.com/MaximilianMcC/Brian", row=0))
+
+    # Send the embed
+    await ctx.respond(embed=embed, view=view)
+
 # Start Brian
 print("Loading...")
 brian.run(config["token"])
